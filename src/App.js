01d126f436a776d280980753431e759cad2abf5e
Paz-Jeremy <146132683+Paz-Jeremy@users.noreply.github.com>
2025-04-26 15:43:13 -0600

    Creando un proyecto de React

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function App() {
+
+    return (
+        <div>
+            <h1>Hola mundo desde React</h1>
+            <p>Creando mi primera aplicacion de react</p>
+        </div>
+        
+    );
+    
+}
