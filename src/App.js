97c4480d7d1249a7f45363253fd437633db3914e
Jeremy <jeremyjosecastellanos@gmail.com>
2025-05-03 15:15:16 -0600

    Modificacion de clase semana #2

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App(){
+
+    return(
+        <div>
+            <h1>Hola Mundo desde react</h1>
+            <p>Creando mi primera aplicacion desde React</p>
+        </div>
+    );
+}
+
+export default App;
