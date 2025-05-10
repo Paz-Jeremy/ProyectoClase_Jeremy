ed8cd7c98c1582e8720ac4c30a313064333023f1
Jeremy <jeremyjosecastellanos@gmail.com>
2025-05-10 13:29:26 -0600

    Avances semana #2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import TopNavbar from './components/TopNavbar'
+import SideNavbar from './components/SideNavbar';
 
 function App(){
 
     return(
-        <div>
-            <h1>Hola Mundo desde react</h1>
-            <p>Creando mi primera aplicacion desde React</p>
-        </div>
+        <Router>
+            <Routes>
+                <Route path="/*" element={
+                    <div>
+                        <TopNavbar />
+                        <SideNavbar />
+                    </div>
+                }/>
+            </Routes>
+        </Router>
     );
 }
 
