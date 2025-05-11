92cae6f838a072fc03bb4d999ac16050bad5e9c8
Jeremy <jeremyjosecastellanos@gmail.com>
2025-05-10 21:18:51 -0600

    Avances clase #3

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import TopNavbar from './components/TopNavbar'
 import SideNavbar from './components/SideNavbar';
+import Login from './pages/Login';
+import Home from './pages/Home';
 
-function App(){
-
-    return(
+function App() {
+    const [items, setItems] = useState([{title:"Progra Web", 
+        description:"Clase de los sabados"}]);
+    return (
         <Router>
             <Routes>
+                <Route path="/login" element={<Login />} />
                 <Route path="/*" element={
                     <div>
                         <TopNavbar />
                         <SideNavbar />
+                        <div style={{ marginLeft: '300px', marginTop: '56px', 
+                            padding: '20px', backgroundColor:'red' }}>
+                            <Routes>
+                                <Route path="/home" element={<Home items={items} />} />
+                            </Routes>
+                        </div>
                     </div>
-                }/>
+                } />
             </Routes>
         </Router>
     );
--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,10 +1,9 @@
-function CardItem({title, description}){
-    return(
-        <div className="card" style="width: 18rem;">
-            {/* <img src="..." className="card-img-top" alt="..."/> */}
+function CardItem({ title, description }) {
+    return (
+        <div className="card h-100">
             <div className="card-body">
-                <h5 className="card-title"> {title} </h5>
-                <p className="card-text"> {description} </p>
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">{description}</p>
             </div>
         </div>
     );
